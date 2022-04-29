f9434355be980a88fa2f461809382777bd13cea4
apache18 <savel.max2003@yandex.ru>
2022-04-29 21:31:03 +0400

    Test
    Test

---
+++ Test/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;  
+
+public class Player : MonoBehaviour
+{
+    private float _speed = 2;
+
+    float hor, ver;
+
+    public Joystick joystick;
+
+    public LayerMask layerMask;
+
+    private NavMeshAgent meshAgent;
+
+    GameObject obj;
+
+    BoxCollider boxCollider;
+   
+    private void Start()
+    {
+        meshAgent = GetComponent<NavMeshAgent>();
+        boxCollider = GetComponent<BoxCollider>();
+    }    
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 200, layerMask))
+            {       
+                if (hit.collider == null) { return; }
+                if (obj) { Destroy(obj);}
+
+                obj = Instantiate(Resources.Load("Point"), hit.point, Quaternion.identity) as GameObject;
+
+                meshAgent.enabled = true;
+                boxCollider.enabled = false;
+                meshAgent.SetDestination(hit.point);
+            }
+        }
+        else
+        {
+            ver = joystick.Vertical * _speed * Time.deltaTime;
+            hor = joystick.Horizontal * _speed * Time.deltaTime;
+
+            if (hor > 0 || ver > 0 || hor < 0 || ver < 0)
+            {
+                meshAgent.enabled = false;
+                boxCollider.enabled = true;
+                Destroy(obj);
+                transform.Translate(new Vector3(hor, 0, ver));
+            }
+        }
+    }
+}
