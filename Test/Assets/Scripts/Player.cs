7c32988988890d70cced7be47693090382e4ff61
apache18 <93821387+apache18@users.noreply.github.com>
2022-05-12 16:09:30 +0400

    Update Player.cs

--- Test/Assets/Scripts/Player.cs
+++ Test/Assets/Scripts/Player.cs
@@ -1,31 +1,51 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;  
 
 public class Player : MonoBehaviour
 {
-    private float _speed = 2;
+    float _speed = 2;
 
-    float hor, ver;
+    float _hor, _ver;
 
     public Joystick joystick;
 
     public LayerMask layerMask;
 
-    private NavMeshAgent meshAgent;
+    NavMeshAgent _meshAgent;
 
-    GameObject obj;
+    GameObject _obj;
 
-    BoxCollider boxCollider;
+    BoxCollider _boxCollider;
    
     private void Start()
     {
-        meshAgent = GetComponent<NavMeshAgent>();
-        boxCollider = GetComponent<BoxCollider>();
-    }    
+        _meshAgent = GetComponent<NavMeshAgent>();
+        _boxCollider = GetComponent<BoxCollider>();
+    }
 
-    void FixedUpdate()
+    void Update()
+    {
+        MoveWithFlag();
+        MoveWithJoystick();
+    }
+
+    void MoveWithJoystick()
+    {
+        _ver = joystick.Vertical * _speed * Time.deltaTime;
+        _hor = joystick.Horizontal * _speed * Time.deltaTime;
+
+        if (_hor > 0 || _ver > 0 || _hor < 0 || _ver < 0)
+        {
+            _meshAgent.enabled = false;
+            _boxCollider.enabled = true;
+            Destroy(_obj);
+            transform.Translate(new Vector3(_hor, 0, _ver));
+        }
+    }
+
+    void MoveWithFlag()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -33,29 +53,17 @@ public class Player : MonoBehaviour
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, 200, layerMask))
-            {       
-                if (hit.collider == null) { return; }
-                if (obj) { Destroy(obj);}
-
-                obj = Instantiate(Resources.Load("Point"), hit.point, Quaternion.identity) as GameObject;
-
-                meshAgent.enabled = true;
-                boxCollider.enabled = false;
-                meshAgent.SetDestination(hit.point);
-            }
-        }
-        else
-        {
-            ver = joystick.Vertical * _speed * Time.deltaTime;
-            hor = joystick.Horizontal * _speed * Time.deltaTime;
-
-            if (hor > 0 || ver > 0 || hor < 0 || ver < 0)
             {
-                meshAgent.enabled = false;
-                boxCollider.enabled = true;
-                Destroy(obj);
-                transform.Translate(new Vector3(hor, 0, ver));
+                if (hit.collider == null) { return; }
+                if (_obj) { Destroy(_obj); }
+
+                _obj = Instantiate(Resources.Load("Point"), hit.point, Quaternion.identity) as GameObject;
+
+                _meshAgent.enabled = true;
+                _boxCollider.enabled = false;
+                _meshAgent.SetDestination(hit.point);
             }
         }
     }
 }
+
